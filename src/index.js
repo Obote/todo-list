7ff77b90dea9a9bb9ef966c39cbf63be91237d5b
Obote <obotedenis256@gmail.com>
2023-07-20 14:07:56 +0300

    Fix Javascript errors

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Todo from './todo.js';
 const display = new Display();
 
 document.querySelector('.AddTaskBtn').addEventListener('click', (e) => {
+  e.preventDefault();
   const listItem = document.getElementById('newTaskID').value;
   const item = new Todo(listItem);
 
